35414f12ae088db12f930a7dd1b4c999cfcb0f21
Jonah Bitautas <jonahvsweb@users.noreply.github.com>
2022-07-16 23:30:07 -0500

    Add create property form.

---
+++ pages/api/homes.js
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      const { image, title, description, price, guests, beds, baths } =
+        req.body;
+
+      const home = await prisma.home.create({
+        data: {
+          image,
+          title,
+          description,
+          price,
+          guests,
+          beds,
+          baths,
+        },
+      });
+      res.status(200).json(home);
+    } catch (e) {
+      res
+        .status(500)
+        .json({
+          message:
+            "Could not save new home. Please try again or contact customer support if the problem continues.",
+        });
+    }
+  } else {
+    res.setHeader("Allow", ["POST"]);
+    res
+      .status(405)
+      .json({ message: "HTTP method ${req.method} is not supported." });
+  }
+}
